cbb9871d352804fc0b76c90df9906640377ae52f
holyzheng <holzpps@163.com>
2018-09-10 16:14:31 +0800

    unless some api

--- server/controllers/public.js
+++ server/controllers/public.js
@@ -5,6 +5,12 @@ const controllerFactory = function (method) {
   return async ctx => {
     let data
     try {
+      ctx.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Content-Type, Content-Length, Authorization, withCredentials, Accept',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS'
+      })
       // 根据对应的 method 执行对应 services 接口
       data = await experimentsServices[method](ctx)
       ctx.body = data
@@ -14,14 +20,29 @@ const controllerFactory = function (method) {
   }
 }
 
+const controllerForOption = function () {
+  return ctx => {
+    ctx.set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Headers': 'Content-Type, Content-Length, Authorization, withCredentials, Accept',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    })
+    ctx.status = 200
+  }
+}
+
 const getAllExperiments = controllerFactory('getAllExperiments')
 
 const getExperiment = controllerFactory('getExperiment')
 
 const getExperimentsByType = controllerFactory('getExperimentsByType')
 
+const corsOption = controllerForOption()
+
 export default {
   getAllExperiments,
   getExperiment,
-  getExperimentsByType
+  getExperimentsByType,
+  corsOption
 }
--- server/routers/public.js
+++ server/routers/public.js
@@ -6,5 +6,8 @@ let publicRouter = new Router()
 publicRouter.get('/experiments', publicController.getAllExperiments)
 publicRouter.get('/experiments/:experiment_id', publicController.getExperiment)
 publicRouter.get('/experiment_types/:type', publicController.getExperimentsByType)
+publicRouter.options('/experiments', publicController.corsOption)
+publicRouter.options('/experiments/:experiment_id', publicController.corsOption)
+publicRouter.options('/experiment_types/:type', publicController.corsOption)
 
 export default publicRouter
